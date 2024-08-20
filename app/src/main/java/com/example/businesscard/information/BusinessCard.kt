2f4e5fc696abcfa5a911b63d70c781dbe91674d4
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-08-19 23:29:01 -0400

    Update Profile component text style and font

--- app/src/main/java/com/example/businesscard/information/BusinessCard.kt
+++ app/src/main/java/com/example/businesscard/information/BusinessCard.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.businesscard.R
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 @Composable
@@ -17,7 +19,8 @@ fun BusinessCard(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
-        Profile("Fernando Peguero", "Software Engineer", Modifier.weight(1f))
+        Profile(stringResource(R.string.name),
+            stringResource(R.string.profession), Modifier.weight(1f))
         ContactInfo(Modifier.weight(1f))
     }
 }
--- app/src/main/java/com/example/businesscard/information/Profile.kt
+++ app/src/main/java/com/example/businesscard/information/Profile.kt
@@ -1,15 +1,21 @@
 package com.example.businesscard.information
 
+import android.graphics.Color.rgb
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.R
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
@@ -25,11 +31,15 @@ fun Profile(name: String, profession: String, modifier: Modifier) {
     ) {
         Image(painter = logo, contentDescription = null)
         Text(
-            text = name
+            text = name,
+            fontSize = 36.sp,
+            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
         )
 
         Text(
-            text = profession
+            text = profession,
+            fontSize = 20.sp,
+            color = Color(rgb(127, 161, 195))
         )
     }
 }
@@ -39,6 +49,8 @@ fun Profile(name: String, profession: String, modifier: Modifier) {
 @Composable
 fun ProfilePreview() {
     BusinessCardTheme {
-        Profile("Fernando Peguero", "Software Engineer", Modifier.fillMaxSize())
+        Profile(
+            stringResource(R.string.name),
+            stringResource(R.string.profession), Modifier.fillMaxSize())
     }
 }
